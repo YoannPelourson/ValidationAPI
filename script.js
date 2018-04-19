6f3b8938d2ab091a9933cebf408e28cd580797a6
admin <yoann.pelourson@gmail.com>
2018-04-19 17:25:36 +0200

    little correction cause the last version doesn't work

--- script.js
+++ script.js
@@ -37,24 +37,25 @@ $(document).ready(function() {
     });
  });
 
- function describeBeer(beerData){
-   let beerID = beerData[i].id;
+ //function describeBeer(beerData){
+   //let beerID = beerData[i].id;
+   
   $('body').on('click', '.beerList', function(){
-    debugger;
-    $.get(('https://api.punkapi.com/v2/beers/ ' + beerID), function(beerData){
+   
+    $.get(('https://api.punkapi.com/v2/beers/ ' ), function(beerInfo){
      let container = document.createElement('div');
-     for (let i = 0; i < beerData.length; i++) {
+     for (let i = 0; i < beerInfo.length; i++) {
        let abvP = document.createElement('p');
        let tipsP = document.createElement('p');
        let yearP = document.createElement('p');
        let descriptionP = document.createElement('p');
        let foodP = document.createElement('p');
  
-        abvP.innerHTML = "Degré d'alcool :" + beerData[i].abv;
-       let tips = "Conseils du brasseur :" + beerData[i].brewers_tips;
-       let year = "Date de 1er brassage :" + beerData[i].first_brewed;
-       let description = "Details :" + beerData[i].description;
-       let food = "Que manger avec :" + beerData[i].food_pairing;
+        abvP.innerHTML = "Degré d'alcool :" + beerInfo[i].abv;
+       let tips = "Conseils du brasseur :" + beerInfo[i].brewers_tips;
+       let year = "Date de 1er brassage :" + beerInfo[i].first_brewed;
+       let description = "Details :" + beerInfo[i].description;
+       let food = "Que manger avec :" + beerInfo[i].food_pairing;
  
        document.querySelector('#resultList').appendChild(abvP);
        
@@ -63,7 +64,7 @@ $(document).ready(function() {
     })
     
   });
- }
+ //}
  
   
 });
